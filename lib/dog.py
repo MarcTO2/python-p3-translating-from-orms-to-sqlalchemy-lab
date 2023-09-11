27c3e21d1ddd5247f374aec02c88e5f4cffe9b38
Absalom <mrclabsalom@gmail.com>
2023-09-11 20:15:34 +0300

    Complete

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,48 @@
+from sqlalchemy import create_engine
 from models import Dog
 
+# Create an in-memory SQLite database
+engine = create_engine('sqlite:///:memory:')
+
 def create_table(base):
-    pass
+    # Create tables based on the models defined in 'models.py'
+    base.metadata.create_all(engine)
+    return engine
 
 def save(session, dog):
-    pass
+    # Add a dog object to the session and commit changes to the database
+    session.add(dog)
+    session.commit()
+    return session
+
+def new_from_db(session, row):
+    # Retrieve a dog from the database based on its ID and return it
+    query = session.query(Dog).filter_by(id=row.id).first()
+    return query
 
 def get_all(session):
-    pass
+    # Retrieve all dog records from the database and return them
+    query = session.query(Dog).all()
+    return query
 
 def find_by_name(session, name):
-    pass
+    # Find a dog by name and return it
+    query = session.query(Dog).filter_by(name=name).first()
+    return query
 
 def find_by_id(session, id):
-    pass
+    # Find a dog by ID and return it
+    query = session.query(Dog).filter_by(id=id).first()
+    return query
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    # Find a dog by name and breed, and return it
+    query = session.query(Dog).filter_by(name=name, breed=breed).first()
+    return query
 
 def update_breed(session, dog, breed):
-    pass
+    # Update the breed of a dog in the database
+    query = session.query(Dog).filter_by(id=dog.id).first()
+    query.breed = breed
+    session.commit()
+    return session
